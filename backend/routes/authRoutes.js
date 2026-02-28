98f787085d9e7eeaf1e99efba53f328f8cbcbe64
kiakioo <ashfaashari@gmail.com>
2026-02-28 23:03:57 +0800

    Memisahkan Vercel Config Frontend & Backend + Fix Package Backend

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const { login, register, getMe } = require('../controllers/authController');
 const { verifyToken, verifyAdmin } = require('../middleware/authMiddleware');
 
-// Hanya berisi rute untuk Otentikasi
 router.post('/login', login);
 router.post('/register', verifyToken, verifyAdmin, register);
 router.get('/me', verifyToken, getMe);
