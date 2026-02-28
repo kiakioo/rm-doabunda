9473eece3e321afe9ce8dbfe944dad6070c97059
kiakioo <ashfaashari@gmail.com>
2026-02-28 14:39:55 +0800

    Fix frontend BASE_URL API to backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,9 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL =
-  import.meta.env.MODE === 'development'
-    ? 'http://localhost:5000/api'
-    : 'https://rm-doabunda.vercel.app/api';
+const BASE_URL = 'https://rm-doabunda.vercel.app/api';
 
 const api = axios.create({
   baseURL: BASE_URL,
